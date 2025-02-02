20355e43aa48dee8d2d00b0fc29789afcb67804b
Karthik Ravichandran <tkgravikarthik@gmail.com>
2025-02-02 11:26:21 -0500

    v0.1.4

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="llm_batch_processor",
-    version="0.1.3",
+    version="0.1.4",
     author="Karthik Ravichandran",
     author_email="tkgravikarthik@gmail.com",
     description="A package to process CSV text data in batches using OpenAI API",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/llm_batch_processor",
+    url="https://github.com/karthikRavichandran/llm_batch_processor",
     packages=find_packages(),
     install_requires=[
         "openai",
